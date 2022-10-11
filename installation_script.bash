e84fe0f34580b830e635f67d6241bbd3e90d3da9
Nynra <nynradev@pm.me>
2022-10-11 15:57:32 +0200

    added an install script

---
+++ installation_script.bash
@@ -0,0 +1,6 @@
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtqui4
+sudo apt-get install libqt4-test 
