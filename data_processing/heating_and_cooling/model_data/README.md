9639d034b295d8d3a270df408ebb321a35cd10fc
Nynra <nynradev@pm.me>
2023-01-12 20:55:28 +0100

    Moved the data processing forlder to report repo

--- data_processing/heating_and_cooling/model_data/README.md
+++
@@ -1,4 +0,0 @@
-# MODEL DATA
-This folder contains data from the dissipation simulation.
-
-The used parameters can be seen in the GridParameters class in the model.py file.
--- data_processing/stage_vibration/vibration_analysis.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from quick_fft_filter import quick_process
-
-
-filename = 'vibration_isolation\data.csv'
-data = np.loadtxt(filename, delimiter=',', skiprows=0)
-results = quick_process(data=data[:, 0], samplerate=30)
